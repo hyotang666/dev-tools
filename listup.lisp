8a19526cf03731e7048835d0fdb1b9ac152f6b6d
Shinichi Sato <hyotang.karakoma@gmail.com>
2019-04-20 21:51:22 +0900

    Improve: LISTUP
    Old:	Not support optional parameter TARGET.
    Issue:	Too many outputs.
    New:	Support it.

--- listup.lisp
+++ listup.lisp
@@ -1,28 +1,41 @@
 (in-package :dev-tools)
 
-(defun listup(package)
+(defun listup(package &optional target)
   (do-external-symbols(s package)
-    (format t "~&~%~A~@[ ~A~]"
-	    (cl-ansi-text:yellow(princ-to-string s))
-	    (cond
-	      ((and (fboundp s)
-		    (not(special-operator-p s)))
-	       (format nil "~:S~%~@[~A~]"
-		       (millet:lambda-list s)
-		       (documentation s 'function)))
-	      ((or (millet:special-symbol-p s)
-		   (constantp s))
-	       (format nil "~A~%~@[~A~]"
-		       (if(boundp s)
-			 (prin1-to-string(symbol-value s))
-			 "; Unbound")
-		       (documentation s 'variable)))
-	      (t (let((class(find-class s nil)))
-		   (when class
-		     (format nil "; of type ~A~%~@[~A~]"
-			     (class-name (class-of class))
-			     (documentation s 'type))))))))
+    (let((roll(symbol-roll s)))
+      (when(or (null target)
+	       (eq roll target))
+	(format t "~&~%~A~@[ ~A~]"
+		(cl-ansi-text:yellow(princ-to-string s))
+		(case roll
+		  (function
+		   (format nil "~:S~%~@[~A~]"
+			   (millet:lambda-list s)
+			   (documentation s 'function)))
+		  (variable
+		   (format nil "; = ~A~%~@[~A~]"
+			   (if(boundp s)
+			     (prin1-to-string(symbol-value s))
+			     "; Unbound")
+			   (documentation s 'variable)))
+		  (type
+		    (format nil "; of type ~A~%~@[~A~]"
+			    (class-name (class-of (find-class s)))
+			    (documentation s 'type)))
+		  ((nil) nil))))))
   (values))
+
+(defun symbol-roll(s)
+  (cond
+    ((and (fboundp s)
+	  (not(special-operator-p s)))
+     'function)
+    ((or (millet:special-symbol-p s)
+		       (constantp s))
+     'variable)
+    ((find-class s nil)
+     'type)
+    (t nil)))
 
 (defun list-all-packages(symbol)
   (loop :for package :in (cl:list-all-packages)
