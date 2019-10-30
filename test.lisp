251bb572de577d105c1db9072381747785df096b
Shinichi Sato <hyotang.karakoma@gmail.com>
2019-10-30 17:18:55 +0900

    Fix: CHECK to better output.

--- test.lisp
+++ test.lisp
@@ -23,21 +23,28 @@
   (dolist(impl *ros-installed*)
     (format t "~&Run test ~S on ~A~%" system impl)
     (force-output)
-    (uiop:run-program
-      (format nil "ros -e '~S' -L ~A"
-	      `(progn
-		 (format t "~A~A~%"
-			 (lisp-implementation-type)
-			 (lisp-implementation-version))
-		 (if (find-package :uiop)
-		   (let(*compile-print* *compile-verbose*)
-		     (asdf:test-system ,system))
-		   (write-line ,(format nil "~A Give up to test ~S because UIOP missing."
-					impl system))))
-	      impl)
-      :output T
-      :error-output *standard-output*
-      :ignore-error-status t)))
+    (multiple-value-bind(out error-out status)
+      (uiop:run-program
+	(format nil "ros -e '~S' -L ~A"
+		`(progn
+		   (format t "~A~A~%"
+			   (lisp-implementation-type)
+			   (lisp-implementation-version))
+		   (if (find-package :uiop)
+		     (let(*compile-print* *compile-verbose*)
+		       (asdf:test-system ,system))
+		     (write-line ,(format nil "~A Give up to test ~S because UIOP missing."
+					  impl system))))
+		impl)
+	:output T
+	:error-output *standard-output*
+	:ignore-error-status t)
+      (declare(ignore out error-out))
+      (unless(zerop status)
+	(format t "~%~A Give up to test ~S: ~S"
+		impl
+		system
+		status)))))
 
 (defun rec-test(system)
   (dolist(system(all-dependencies system))
